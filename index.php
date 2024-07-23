422a007685d8c42e0ce30974f3cb5c20e72fe7f9
AnanditaDev <ananditadev4@gmail.com>
2024-07-23 02:37:40 -0700

    adding index.php file

---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shops</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
+    integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" 
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="CSS/header.css"/>
+    <link rel="stylesheet" href="CSS/shops.css"/>
+
+</head>
+<body>
+    <?php
+        include "includes/header.php";
+    ?>
+
+    <div class="trend">
+        <div class="head">
+            <h1>Trends <span>Men's Wear</span></h1>
+        </div>
+        <div class="card">
+            <div class="crd">
+                <img src="https://m.theblockart.com/ImageStorage/BA2008052022222225774829.jpeg" alt="">
+                <div class="crdText">
+                    <h2>Cotton Trending shirt's</h2>
+                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                    <i class="fa-solid fa-star-half-stroke" style="color: #FFD43B;"></i>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+            <div class="crd">
+                <img src="https://images-na.ssl-images-amazon.com/images/I/71w5T1ZLAbL._AC_UL1206_.jpg" alt="">
+                <div class="crdText">
+                    <h2>Cotton Trending shirt's</h2>
+                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                    <i class="fa-solid fa-star-half-stroke" style="color: #FFD43B;"></i>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+            <div class="crd">
+                <img src="https://images-na.ssl-images-amazon.com/images/I/71dBglMSxdS._AC_UL1500_.jpg" alt="">
+                <div class="crdText">
+                    <h2>Cotton Trending shirt's</h2>
+                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                    <i class="fa-solid fa-star-half-stroke" style="color: #FFD43B;"></i>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Women section -->
+
+    <div class="trend">
+        <div class="head">
+            <h1>Trends <span>Women's Wear</span></h1>
+        </div>
+        <div class="card">
+            <div class="crd">
+                <img src="https://storage.googleapis.com/cherri-lk-prod/uploads/products/preview/6597941e9e1d2.webp" alt="">
+                <div class="crdText">
+                    <h2>Trending Girls Top</h2>
+                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                    <i class="fa-solid fa-star-half-stroke" style="color: #FFD43B;"></i>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+            <div class="crd">
+                <img src="https://www.sanasafinaz.com/media/catalog/product/s/s/ss21sge375.jpg?optimize=medium&bg-color=255,255,255&fit=bounds&height=600&width=388&canvas=388:600" alt="">
+                <div class="crdText">
+                    <h2>Trending Girls Top</h2>
+                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                    <i class="fa-solid fa-star-half-stroke" style="color: #FFD43B;"></i>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+            <div class="crd">
+                <img src="https://images.petallush.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/b/r/brld0053-3.jpg" alt="">
+                <div class="crdText">
+                    <h2>Trending Girls Top</h2>
+                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                    <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                    <i class="fa-solid fa-star-half-stroke" style="color: #FFD43B;"></i>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
